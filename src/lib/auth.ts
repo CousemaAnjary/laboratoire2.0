06c2ab93d7f6e6692a9ef98593b1d7e00203ebb1
CousemaAnjary <anjaryabdillah@gmail.com>
2025-01-25 00:10:06 +0300

    Mise à jour de la configuration d'authentification NextAuth avec le fournisseur Github et ajout d'un nouveau fichier de route API pour gérer les requêtes d'authentification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,16 @@
 import NextAuth from "next-auth"
 import { prisma } from './prisma'
-import Github from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
-    providers: [Github],
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        // ...add more providers here
+    ],
 })

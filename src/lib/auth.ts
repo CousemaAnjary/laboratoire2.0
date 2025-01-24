d6b40f2c75998cc0d33d21b37755c57ecfb1fde7
CousemaAnjary <anjaryabdillah@gmail.com>
2025-01-25 00:22:37 +0300

    Réorganisation des imports dans le fichier auth.ts pour améliorer la lisibilité

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth"
 import { prisma } from './prisma'
-import GithubProvider from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-
+import GithubProvider from "next-auth/providers/github"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),

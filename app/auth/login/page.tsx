23b46f064ef64ad6054bab167ade6eb0f19fe258
CousemaAnjary <anjaryabdillah@gmail.com>
2025-01-25 17:32:50 +0300

    Ajout du composant GridPattern et de la Navbar à la page de connexion, réorganisation de la structure du rendu pour améliorer l'affichage.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,19 +1,44 @@
-export default function page() {
+import { cn } from "@/src/lib/utils"
+import Navbar from "@/src/components/Navbar"
+import { GridPattern } from "@/src/components/ui/grid-pattern"
+
+
+export default function Login() {
     /**
      * ! STATE (état, données) de l'application
      */
-    
-    
+
+
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-    
-    
+
+
     /**
      * ! AFFICHAGE (render) de l'application
      */
     return (
         <>
+            <div className="flex flex-col min-h-screen ">
+                <GridPattern width={50} height={50} x={-1} y={-1} strokeDasharray={"4 2"} className={cn("absolute inset-0 [mask-image:radial-gradient(900px_circle_at_center,white,transparent)]")} />
+                {/* <Toaster richColors /> */}
+                {/* En-tête */}
+                <header>
+                    <Navbar />
+                </header>
+
+                {/* Contenu principal */}
+                <main className="flex-grow">
+                    {/* Section 1 */}
+                    <section className="flex justify-center items-center min-h-[84vh]">
+                        {/*  container du formulaire de connexion */}
+                        {/* <LoginForm /> */}
+                    </section>
+                </main>
+
+                {/* Pied de page */}
+                <footer></footer>
+            </div>
         </>
     )
 }

9eb16229a99fe6306e2057f81024a32f83b5837f
CousemaAnjary <anjaryabdillah@gmail.com>
2025-01-25 21:12:47 +0300

    Mise à jour de la dépendance 'next-auth' et refactorisation de la configuration d'authentification pour utiliser les options de NextAuth.import { authOptions } from "@/src/lib/auth"

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,26 @@
-import NextAuth from "next-auth"
+import { NextAuthOptions } from "next-auth"
 import { prisma } from './prisma'
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import GithubProvider from "next-auth/providers/github"
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//     adapter: PrismaAdapter(prisma),
+//     providers: [
+//         GithubProvider({
+//             clientId: process.env.GITHUB_ID,
+//             clientSecret: process.env.GITHUB_SECRET,
+//         }),
+//         // ...add more providers here
+//     ],
+// })
+
+export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
         }),
         // ...add more providers here
     ],
-})
+}

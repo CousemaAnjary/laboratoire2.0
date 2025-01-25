2c59dc09089dcadf42eeca0e310c30921d5e4707
CousemaAnjary <anjaryabdillah@gmail.com>
2025-01-25 19:21:48 +0300

    Désactivation de la règle 'react/no-unescaped-entities' dans la configuration ESLint pour éviter les avertissements inutiles.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,11 @@ const eslintConfig = [
     "next/typescript",
     "plugin:tailwindcss/recommended",
   ),
+  {
+    rules: {
+      "react/no-unescaped-entities": "0",
+    },
+  }
 ];
 
 export default eslintConfig;
